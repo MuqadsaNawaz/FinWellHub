0ad1c7bc94722b9fcc6fc0adb68c5edbfbb73a39
Admin <Admin@MUQADSA>
2024-05-28 23:16:12 -0700

    Add project files.

---
+++ FinWell Hub Web Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinWell_Hub_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
